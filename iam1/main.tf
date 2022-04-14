010e3bec23b3d1906f5f3e3b270bc06eaae4f1dc
practiceaws031 <103592920+practiceaws031@users.noreply.github.com>
2022-04-14 07:48:30 +0530

    Update main.tf

--- iam1/main.tf
+++ iam1/main.tf
@@ -2,11 +2,19 @@ variable "create_user"{
     type=bool
     default=true
 }   
+variable "name"{
+    type=string
+    default=""
+}
 
+variable "path"{
+   type=string
+    default="/"
+}
 resource "aws_iam_user" "u1" {
     count=var.create_user?1:0
-    name="dfg12"
-    path="/"
+    name=var.name
+    path=var.path
 }
 
 output "user_name" {
